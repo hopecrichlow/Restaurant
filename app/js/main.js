e1e103a2408f6528da33f20cb560231e841b8035
Jesse Soldat <jsoldat@hotmail.com>
2015-10-15 13:45:20 -0400

    finished the tab section

--- app/js/main.js
+++ app/js/main.js
@@ -3,4 +3,17 @@
 (function () {
 
   console.log('It Works!');
+
+  $(document).ready(function () {
+
+    $('ul.tabs li').click(function () {
+      var tab_id = $(this).attr('data-tab');
+
+      $('ul.tabs li').removeClass('open');
+      $('.tabContent').removeClass('open');
+
+      $(this).addClass('open');
+      $("#" + tab_id).addClass('open');
+    });
+  });
 })();
--- src/main.js
+++ src/main.js
@@ -1,6 +1,22 @@
 (function () {
 
   console.log('It Works!');
-  console.log('test');
+
+
+
+
+  $(document).ready(function(){
+  
+  $('ul.tabs li').click(function(){
+    var tab_id = $(this).attr('data-tab');
+
+    $('ul.tabs li').removeClass('open');
+    $('.tabContent').removeClass('open');
+
+    $(this).addClass('open');
+    $("#"+tab_id).addClass('open');
+  })
+
+})
 
 }());
